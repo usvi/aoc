e274c4a6d0a897d6d65bcf8459d833f0697f0853
Janne Paalijarvi <janne.paalijarvi@efore.com>
2021-12-02 07:37:09 +0200

    Made template simpler and now takes input file name from command line.

--- 2021/00_template/aoc2.c
+++ 2021/00_template/aoc2.c
@@ -4,33 +4,51 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
-
-
-int64_t i64ProcessLine(int64_t i64ParamLine)
-{
-  return i64ParamLine;
-}
+#include <string.h>
 
 
 int main(int argc, char *argv[])
 {
   char sLineBuf[LINE_BUF_SIZE] = { 0 };
-  int64_t i64TotalSum = 0;
   int64_t i64LineInput = 0;
 
-  FILE* pxFile = fopen("input.txt", "r");
+  int64_t i64Forward = 0;
+  int64_t i64Depth = 0;
+  int64_t i64Aim = 0;
+
+  FILE* pxFile = fopen(argv[argc - 1], "r");
 
   if (pxFile == NULL)
   {
+    printf("Unable to open input file %s\n", argv[argc - 1]);
+    
     return 1;
   }
   while(fgets(sLineBuf, sizeof(sLineBuf) , pxFile) != NULL)
   {
-    i64LineInput = atoll(sLineBuf);
-    i64TotalSum += i64ProcessLine(i64LineInput);
+    if (strstr(sLineBuf, "forward") == sLineBuf)
+    {
+      i64LineInput = atoll(strstr(sLineBuf, " ") + 1);
+      i64Forward += i64LineInput;
+      i64Depth += i64LineInput * i64Aim;
+    }
+    else if (strstr(sLineBuf, "down") == sLineBuf)
+    {
+      i64LineInput = atoll(strstr(sLineBuf, " ") + 1);
+      i64Aim += i64LineInput;
+    }
+    else if (strstr(sLineBuf, "up") == sLineBuf)
+    {
+      i64LineInput = atoll(strstr(sLineBuf, " ") + 1);
+      i64Aim -= i64LineInput;
+    }
+    
   }
   fclose(pxFile);
-  printf(">%" PRIi64 "\n", i64TotalSum);
+
+  //printf(">%" PRIi64 "\n", i64TotalSum);
+
+  printf(">%" PRIi64 "  %" PRIi64 "  %" PRIi64 "\n", i64Forward, i64Depth, (i64Forward * i64Depth));
 
   return 0;
 }
